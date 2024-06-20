e6a643028c194894cf729f6a31c32471a569ed23
Eunice <eunhye0320@gmail.com>
2024-06-20 16:33:04 -0700

    update

---
+++ spring-boot3-with-docker-n-validation/src/main/java/com/example/spring_boot3_with_docker_n_validation/SpringBoot3WithDockerNValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot3_with_docker_n_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3WithDockerNValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3WithDockerNValidationApplication.class, args);
+	}
+
+}
---
+++ spring-boot3-with-docker-n-validation/src/test/java/com/example/spring_boot3_with_docker_n_validation/SpringBoot3WithDockerNValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot3_with_docker_n_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3WithDockerNValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
